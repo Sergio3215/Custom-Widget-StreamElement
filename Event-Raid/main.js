1ab3dab2914fa73a13a233560f80396a81b9ea51
Sergio <principiante.en.programar@gmail.com>
2025-03-12 06:10:16 -0300

    adding security on service and bot channel

--- Event-Raid/main.js
+++ Event-Raid/main.js
@@ -2,27 +2,28 @@
  * @author SerezDev
  * @link https://www.serez.dev/
  * @linkcode https://github.com/Sergio3215/Custom-Widget-StreamElement/tree/main/Event-Raid
- * @description An development for event hen stream has a raid
-*/
+ * @description An development for event hen stream has a raid, you need add to SerezDevBot as moderator
+ */
 
+
+let fieldData;
 
 /**
  * 
  * @param {string} msg Message to show in the chat of stream
  */
+
 async function sendMessage(msg) {
+  //This Function works, you need select the channel
   try {
+    const { Channel } = fieldData;
     const ftch = await fetch('https://service-events-twitch-production.up.railway.app/send-message', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        identity: {
-          username: "Your-Channel",
-          password: "Your-Oauth-Token"
-        },
-        channel: "Your-Channel",
+        channel: Channel,
         message: msg
       })
     });
@@ -41,9 +42,18 @@ window.addEventListener('onEventReceived', async function (obj) {
       name
     } = data;
 
+    //Here Edit the message!
     const msg = `¡Gracias @${name} por la raid de ${amount == 1? ` 1 persona`: `${amount} Personas`}! ${amount == 1? `Bienvenido`: `Bienvenidos`} al canal! 🎉 Siganlo todos! https://www.twitch.tv/${name}`;
+
+    //Here Send the message
     sendMessage(msg);
-    // console.log(msg);
-    // console.log("es una raid")
   }
+});
+
+
+window.addEventListener('onWidgetLoad', function (obj) {
+  if (!obj.detail.fieldData) {
+      return;
+  }
+  fieldData = obj.detail.fieldData;
 });
