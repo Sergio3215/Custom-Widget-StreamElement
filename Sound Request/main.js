7cd232381b447a7c16f4f5f80ab5b1b862aecd27
Sergio <principiante.en.programar@gmail.com>
2026-01-24 00:08:14 -0300

    add sound (music) request for spotify

---
+++ Sound Request/main.js
@@ -0,0 +1,75 @@
+/**
+ const ftch = await fetch('https://service-events-twitch-production.up.railway.app/send-message', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        channel: Channel,
+        message: msg
+      })
+    });
+ */
+
+let fieldData;
+
+const SendMessage = async (Channel, msg) => {
+    const ftch = await fetch('https://service-events-twitch-production.up.railway.app/send-message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            channel: Channel,
+            message: msg
+        })
+    });
+}
+
+const sendQueue = async (objMessage) => {
+
+    const { text } = objMessage;
+
+    if (text.includes(fieldData.Command)) {
+        try {
+
+            setTimeout(() => {
+
+            }, 2000);
+
+
+            const track = text.split(fieldData.Command)[1];
+
+            const ftch = await fetch(`https://sound-request-spotify-production.up.railway.app/change-track?track=${track.trim()}&token=${fieldData.Token}&refreshToken=${fieldData.RefreshToken}`, {
+                method: 'GET',
+            });
+
+            // const data = await ftch.json();
+
+            SendMessage(fieldData.Channel, "Se ha agregado una nueva canción a la cola");
+
+        } catch (error) {
+            SendMessage(fieldData.Channel, "Token vencido o cancion invalido");
+        }
+    }
+}
+
+window.addEventListener('onEventReceived', async function (obj) {
+
+    if (!obj.detail.event) {
+        return;
+    }
+
+    // console.log(obj.detail);
+
+    if (obj.detail.listener == "message") {
+        sendQueue(obj.detail.event.data);
+    }
+});
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    if (!obj.detail.fieldData) {
+        return;
+    }
+    fieldData = obj.detail.fieldData;
+});
